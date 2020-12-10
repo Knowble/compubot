da8c470fb3e63a9b46587c9fa27a1bba60743bc1
Knowble <knowble.off@gmail.com>
2020-12-10 18:54:48 +0700

    fix

--- commands/ig.js
+++ commands/ig.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js')
-const fs = require('fs')
-const ytdl = require('ytdl-core')
+// const Discord = require('discord.js')
+// const fs = require('fs')
+// const ytdl = require('ytdl-core')
  
-module.exports.run = async (client, message, args, blue) => {
-  // var patt = /instagram.com/g
-  // var res = patt.test(args[0]);
-  // if(res == false) return message.reply("Invalid Instagram URL!")
+// module.exports.run = async (client, message, args, blue) => {
+//   var patt = /instagram.com/g
+//   var res = patt.test(args[0]);
+//   if(res == false) return message.reply("Invalid Instagram URL!")
  
 
-}
-//name this whatever the command name is.
-module.exports.help = {
-  name: "ig"
-}
+// }
+// name this whatever the command name is.
+// module.exports.help = {
+//   name: "ig"
+// }
