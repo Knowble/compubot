717656f88b352fa43f2ccf913f6cabe75191d6b0
Knowble <knowble.off@gmail.com>
2020-12-10 19:55:35 +0700

    update

--- commands/yt.js
+++ commands/yt.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
 const fs = require('fs')
-const ytdl = require('ytdl-core')
 const url   = require('url');
 const https = require('https');
 
+const ytdl = require('ytdl-core')
 module.exports.run = async (client, message, args, blue) => {
   function makeid(length) {
     var result           = '';
@@ -16,9 +16,13 @@ module.exports.run = async (client, message, args, blue) => {
  }
   var num = "";
   num = makeid(5);
-  var limit = 1024 * 1024 * 250;
-  var videoID = args[0];
-  let validate = ytdl.validateURL(args[0])
+  if (!args.length) {
+    return message.channel.send(`Specify a format and the URL.`);
+  }
+  else if (args[0] === 'mp4') {
+    var limit = 1024 * 1024 * 250;
+  var videoID = args[1];
+  let validate = ytdl.validateURL(args[1])
   if(validate === false) return message.reply("That is an invalid URL!")
 ytdl.getInfo(videoID).then((info) => {
   var formats = info.formats.slice();
@@ -29,7 +33,7 @@ ytdl.getInfo(videoID).then((info) => {
       if (parseInt(response.headers['content-length']) <= limit) {
         blue.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
         blue.setTimestamp()
-        blue.setDescription(`**${info.player_response.videoDetails.title}** requested by <@${message.author.id}>\nI will DM you when the files are ready!`)
+        blue.setDescription(`**${info.player_response.videoDetails.title}** MP4 requested by <@${message.author.id}>\nI will DM you when the files are ready!`)
         message.channel.send(blue)
         var output = `/var/www/html/assets/cdn/${num}.mp4`;
         ytdl.downloadFromInfo(info, { filter: 'audioandvideo', quality: 'highestvideo' })
@@ -40,8 +44,23 @@ ytdl.getInfo(videoID).then((info) => {
       }
     }).end();
 });
+  return
+  }  else if (args[0] === 'mp3') {
+    let vido = ytdl.getURLVideoID(args[1])
+    await ytdl.getBasicInfo(vido).then(info => {
+    blue.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
+    blue.setTimestamp()
+    blue.setDescription(`**${info.player_response.videoDetails.title}** MP3 requested by <@${message.author.id}>\nI will DM you when the files are ready!`)
+    message.channel.send(blue)
+    });
+    ytdl(`${args[1]}`, { filter: 'audioonly', quality: 'highestaudio' })
+    .pipe(fs.createWriteStream(`/var/www/html/assets/cdn/${num}.mp3`)).on("finish", function() {
+      message.author.send(blue.setDescription(`Here is your file!\nhttps://betterverify.xyz/assets/cdn/${num}.mp3\n\nThis link will expire in 5 minutes.`))
+    });
+    return
+  } 
 }
 //name this whatever the command name is.
 module.exports.help = {
-  name: "ytest"
+  name: "yt"
 }
