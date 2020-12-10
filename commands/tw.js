95749287271be88bf17747e1a9307599964b3bfc
Knowble <knowble.off@gmail.com>
2020-12-10 18:44:03 +0700

    fix

--- commands/tw.js
+++ commands/tw.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js')
-const fs = require('fs')
-const ytdl = require('ytdl-core')
-const mql = require('@microlink/mql')
+const Discord = require('discord.js');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const mql = require('@microlink/mql');
 module.exports.run = async (client, message, args, blue) => {
-  var patt = /twitter.com/g
+  var patt = /twitter.com/g;
   var res = patt.test(args[0]);
-  if(res == false) return message.reply("Invalid Twitter URL!")
+  if(res == false) return message.reply("Invalid Twitter URL!");
   const { status, data, response } = await mql(
     args[0],
     {
@@ -14,8 +14,8 @@ module.exports.run = async (client, message, args, blue) => {
   )
   blue.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
   blue.setTimestamp()
-  message.channel.send(blue.setDescription(`I will DM you when the files are ready!`))
-  message.author.send(blue.setDescription(`Here is your file!\n${data.video.url}`))
+  message.channel.send(blue.setDescription(`I will DM you when the files are ready!`));
+  message.author.send(blue.setDescription(`Here is your file!\n${data.video.url}`));
 }
 //name this whatever the command name is.
 module.exports.help = {
--- commands/yt.js
+++ commands/yt.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const ytdl = require('ytdl-core')
 module.exports.run = async (client, message, args, blue) => {
   function makeid(length) {
-    var result           = '';
+    var result           = "";
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
@@ -15,14 +15,14 @@ module.exports.run = async (client, message, args, blue) => {
   num = makeid(5);
   if(!message.author.id == '760826345965617192') return ;
   let validate = ytdl.validateURL(args[0])
-  if(validate == false) return message.reply("That is an invalid URL!")
+  if(validate === false) return message.reply("That is an invalid URL!")
   let vido = ytdl.getURLVideoID(args[0])
   await ytdl.getBasicInfo(vido).then(info => {
   blue.setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
   blue.setTimestamp()
   blue.setDescription(`**${info.player_response.videoDetails.title}** requested by <@${message.author.id}>\nI will DM you when the files are ready!`)
   message.channel.send(blue)
-  }).catch(console.error);
+  });
   ytdl(`${args[0]}`, { filter: 'audioandvideo', quality: 'highestvideo' })
   .pipe(fs.createWriteStream(`/var/www/html/assets/cdn/${num}.mp4`)).on("finish", function() {
     message.author.send(blue.setDescription(`Here is your file!\nhttps://betterverify.xyz/assets/cdn/${num}.mp4\n\nThis link will expire in 5 minutes.`))
